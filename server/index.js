3b90a4782c50ada7989d0f193214c1adb8145466
Eman Ajanić <ajaniceman@gmail.com>
2025-06-11 01:01:00 +0200

    Server index.js update

--- server/index.js
+++ server/index.js
@@ -39,16 +39,16 @@ app.get('/api/health', async (req, res) => {
 
 // Serve static files from React in production
 if (process.env.NODE_ENV === 'production') {
-    const clientBuildPath = path.join(__dirname, '..', 'client', 'build');
-
+    const clientBuildPath = path.join(__dirname, 'public');    
     app.use(express.static(clientBuildPath));
-
+    
     app.get('/*splat', (req, res) => {
-    res.sendFile(path.join(clientBuildPath, 'index.html'));
-    });
+      res.sendFile(path.join(clientBuildPath, 'index.html'));
+});
+
 }
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
