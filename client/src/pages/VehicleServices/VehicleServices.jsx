fbcd28dcd0fd5eedf08df51399a40f5b0531da2c
Eman Ajanić <ajaniceman@gmail.com>
2025-06-16 18:11:35 +0200

    VehicleServices added to a new folder

--- client/src/pages/VehicleServices/VehicleServices.jsx
+++ client/src/pages/VehicleServices/VehicleServices.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import ServiceForm from '../components/ServiceForm/ServiceForm';
+import ServiceForm from '../../components/ServiceForm/ServiceForm';
 import './VehicleServices.css'; // The CSS file will still be named this
 
 export default function VehicleServices() {
